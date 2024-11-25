870928e2cdfd8f4a530425e006abf178173dc382
valerykharko <x-valery@mail.ru>
2024-11-25 17:36:02 +0300

    fix: changes common-libs.d.ts

--- common-libs.d.ts
+++ common-libs.d.ts
@@ -1,9 +1,19 @@
-declare module 'common-libs' {
+declare module 'common-libs/src' {
   export class JwtService {
-    // Здесь объявите методы и свойства класса JwtService
     sign(payload: object, secret: string): string;
     verify(token: string, secret: string): object;
   }
 
-  // Можете добавлять другие типы или сервисы
+  export class WinstonService {
+    log(message: string): void;
+    error(message: string, trace: string): void;
+    warn(message: string): void;
+    debug(message: string): void;
+    verbose(message: string): void;
+  }
+
+  // Пример для других типов и сервисов
+  export * from './src/guards/jwt-auth.guard';
+  export * from './src/services/logger.service';
+  export * from './src/services/jwt.service';
 }
